30e4e88e491328e4eb87589bb642433db4688b86
PawaniBhagya <pawanibhagya10@gmail.com>
2025-01-18 15:20:27 +0530

    Finalized the full-responsive B-Stylish app with login-logout functions and interconnected frontend, and admin panel with MongoDB.

--- backend/index.js
+++ backend/index.js
@@ -188,8 +188,96 @@ const Users = mongoose.model('Users',{
     })
 
     //Creating EndPoint for Login User
-    
+    app.post('/login',async (req,res)=>{
+        let user = await Users.findOne({email:req.body.email});
+        if (user){
+            const passCompare = req.body.password === user.password;
+            if (passCompare){
+                const data = {
+                    user:{
+                        id:user.id
+                    }
+                }
+                const token = jwt.sign(data,'secret_ecom');
+                res.json({
+                    success:true,
+                    token
+                });
+            }
+            else{
+                res.json({
+                    success:false,
+                    errors:"Password is Incorrect"
+                });
+            }
+        }
+        else{
+            res.json({
+                success:false,
+                errors:"Wrong Email Id"
+            });
+        }
+    })
 
+//Creating EndPoint for New-Collection data
+app.get('/newcollections',async (req,res)=>{
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("New Collection Fetched");
+    res.send(newcollection);
+})
+
+//Creating Endpoint for Popular in Women Section
+app.get('/popularinwomen',async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in Women Fetched");
+    res.send(popular_in_women);
+})
+
+//Creating Middleware to Fetch User
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token');
+    if (!token){
+        res.status(401).send({errors:"Please authenticate using a valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        }catch (error){
+            res.status(401).send({errors:"Please authenticate using a valid token"})
+        }
+    }
+
+}
+
+//Creating EndPoint for Adding Products in Cartdata
+app.post('/addtocart',fetchUser,async (req,res)=>{
+    console.log("added",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] +=1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added")
+})
+
+//Creating EndPoint to remove Product in CartData
+app.post('/removefromcart',fetchUser,async (req,res)=>{
+    console.log("removed",req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Removed")
+})
+
+//Creating EndPoint to get cartdata
+app.post('/getcart',fetchUser,async (req,res)=>{
+    console.log("GetCart");
+    let userData = await Users.findOne({_id:req.user.id});
+    res.json(userData.cartData);
+})
 
 app.listen(port,(error)=>{
     if (!error){
